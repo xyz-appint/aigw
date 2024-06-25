15af3021b48315ca82a0dfa8c0832fed5b866ba4
Justin <justin@appint.cn>
2024-06-25 13:35:47 +0800

    build

--- src/start-server.ts
+++ src/start-server.ts
@@ -5,7 +5,7 @@ import { serve } from '@hono/node-server';
 import app from './index';
 
 // Extract the port number from the command line arguments
-const defaultPort = 8787;
+const defaultPort = 80;
 const args = process.argv.slice(2);
 const portArg = args.find((arg) => arg.startsWith('--port='));
 const port = portArg ? parseInt(portArg.split('=')[1]) : defaultPort;
